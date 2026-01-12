f6174e3da81a77a7a133b4de66ed88316b60e128
Gabriel-Ryu <gabrielryunakaya@gmail.com>
2026-01-11 21:51:23 -0300

    documentacao e ajustes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,25 +8,58 @@ use Illuminate\Http\Request;
 use App\Repository\UserRepo;
 use Exception;
 
+/**
+ * @group Ações do usuário
+ * 
+ */
 class UserController extends Controller
 {
+    /**
+     * Apresenta o usuário logado atualmente
+     * 
+     * @response 200 {
+     *  "data": [{"id": 1, "nome": "Teclado Mecânico", "preco": 250.00}]
+     * }
+     */
+    /**
+     * Apresenta o usuário logado atualmente.
+     * 
+     * @bodyParam email string required Endereço de e-mail cadastrado. Example: joaosilva@email.com
+     * @bodyParam password string required Senha do usuário cadastrado. Example: senha123
+     * 
+     * @response 200 {
+     *  "error": false,
+     *  "message": "Voce nao esta logado na API, favor realizar o login",
+     *  "id": 1,
+     *  "name": "Joao da Silva"
+     *  "email": "joaosilva@email.com"
+     * }
+     * 
+     * @response 401 {
+     *  "error": true,
+     *  "message": "Voce nao esta logado na API, favor realizar o login",
+     *  "id": 1,
+     *  "name": "Joao da Silva"
+     *  "email": "joaosilva@email.com"
+     * }
+     */
     public function checkUser(){
         $user = UserRepo::checkUser();
         if(empty($user)){
-            return [
+            return response()->json([
                 'error' => true,
                 'message' => "Voce nao esta logado na API, favor realizar o login",
                 'id' => "{$user[0]['id']}",
                 'name' => "{$user[0]['name']}",
                 'email' => "{$user[0]['email']}"
-            ];
+            ], 401);
         }
-        return [
+        return response()->json([
             'error' => false,
             'message' => "Voce esta logado na API, email: {$user[0]['email']}",
             'id' => "{$user[0]['id']}",
             'name' => "{$user[0]['name']}",
             'email' => "{$user[0]['email']}"
-        ];
+        ], 200);
     }
 }
--- app/Repository/UserRepo.php
+++ app/Repository/UserRepo.php
@@ -4,38 +4,74 @@ use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Redis;
 
+/**
+ * Class UserRepo
+ * 
+ * Repositório para armazenar queries do banco de dados usando o model User
+ * 
+ * @package App\Repository
+ */
 class UserRepo{
+    /**
+     * Cria usuários seguindo os parêmetros passados.
+     * 
+     * @param array ['name' => 'required|max:255', 'email' => 'required|max:255', 'password' => 'required|string|min:6']
+     * @return @response 201 {
+     *                      "error": false,
+     *                      "message": "Usuário criado com sucesso",
+     *                      "data": {"name": "Joao da Silva", "email": "joaosilva@email.com", "updated_at": "XXXX-XX-XXTXX:XX:XX.XXXXXXZ", "created_at": "XXXX-XX-XXTXX:XX:XX.XXXXXXZ", "id": 1}
+     * }
+     * @response 409 {
+     *                      "error": true,
+     *                      "message": "Este email ja esta em uso"
+     * }
+     * 
+     * @throws \Throwable Caso haja falha na conexão com o banco.
+     */
     public static function create($params){
         try {
-            return User::firstOrCreate($params);
+            $user =  User::firstOrCreate($params);
+            if (!$user->wasRecentlyCreated) {
+                return response()->json([
+                    'error' => true,
+                    'message' => "Este email ja esta em uso",
+                ], 409);
+            }
+
+            return response()->json([
+                'error' => false,
+                'message' => "Usuario criado com sucesso",
+                'data' => $user
+            ], 201);
         } catch (\Throwable $e) {
             $sqlState = $e->getCode();
             $driverCode = $e->errorInfo[1] ?? null;
             if ($sqlState === '23000' && $driverCode === 1062) {
-                return [
+                return response()->json([
+                    'error' => true,
                     'message' => "Este email ja esta em uso",
-                    'errors' => "Ja existe um usuario com este email",
-                    'errorCode' => 422
-                ];
+                ], 409);
             }
-            return [
+            return response()->json([
+                'error' => true,
                 'message' => "Falha ao criar usuario",
                 'detail'  => $e->getMessage(),
-                'errorCode' => 400
-            ];
-
+            ], 500);
         }
     }
 
+    /**
+     * Verifica se o usuário existe, utilizando o redis para encontrar o usuário logado no momento.
+     * 
+     * @param null
+     * @return 0 = ['id' => 1, 'name' => Joao da Silva, 'email' => joaosilva@email.com, 'password' => senhaHashada, 'created_at' => XXXX-XX-XX XX:XX:XX, 'updated_at' => XXXX-XX-XX XX:XX:XX]
+     * 
+     * @throws \Throwable Caso haja falha na conexão com o banco.
+     */
     public static function checkUser(){
         $user = unserialize(Redis::get('user'));
-        return User::where('email', $user->email)
+        return User::where('id', $user)
         ->get()
         ->toArray();
-    }
-
-    public static function getUser($email){
-        return User::where('email', $email)
-        ->first();
     }
 }
